4d04492f97163b0027edc3ca70a856084d64b7d1
zhiwht <251897752@qq.com>
2017-10-17 16:51:00 +0800

    taliking
    vue-iview

---
+++ iview-admin-dev/src/router.js
@@ -0,0 +1,188 @@
+import Main from './views/Main.vue';
+
+export const loginRouter = {
+    path: '/login',
+    name: 'login',
+    meta: {
+        title: 'Login - 登录'
+    },
+    component: resolve => { require(['./views/login.vue'], resolve); }
+};
+
+export const page404 = {
+    path: '/*',
+    name: 'error_404',
+    meta: {
+        title: '404-页面不存在'
+    },
+    component: resolve => { require(['./views/error_page/404.vue'], resolve); }
+};
+
+export const page401 = {
+    path: '/401',
+    meta: {
+        title: '401-权限不足'
+    },
+    name: 'error_401',
+    component: resolve => { require(['./views/error_page/401.vue'], resolve); }
+};
+
+export const page500 = {
+    path: '/500',
+    meta: {
+        title: '500-服务端错误'
+    },
+    name: 'error_500',
+    component: resolve => { require(['./views/error_page/500.vue'], resolve); }
+};
+
+export const otherRouter = {
+    path: '/',
+    name: 'otherRouter',
+    redirect: '/home',
+    component: Main,
+    children: [
+        { path: 'home', title: '首页', name: 'home_index', component: resolve => { require(['./views/home/home.vue'], resolve); } },
+        { path: 'ownspace', title: '个人中心', name: 'ownspace_index', component: resolve => { require(['./views/own-space/own-space.vue'], resolve); } },
+        { path: 'message', title: '消息中心', name: 'message_index', component: resolve => { require(['./views/message/message.vue'], resolve); } }
+    ]
+};
+
+export const preview = {
+    path: '/preview',
+    name: 'preview',
+    component: resolve => { require(['./views/form/article-publish/preview.vue'], resolve); }
+};
+
+export const locking = {
+    path: '/locking',
+    name: 'locking',
+    component: resolve => { require(['./views/main_components/locking-page.vue'], resolve); }
+};
+
+export const appRouter = [
+    {
+        path: '/access',
+        redirect: '/access/index',
+        icon: 'key',
+        name: 'access',
+        title: '权限管理',
+        component: Main,
+        children: [
+            { path: 'index', title: '权限管理', name: 'access_index', component: resolve => { require(['./views/access/access.vue'], resolve); } }
+        ]
+    },
+    {
+        path: '/access-test',
+        icon: 'lock-combination',
+        title: '权限测试页',
+        name: 'accesstest',
+        access: 0,
+        component: Main,
+        children: [
+            { path: 'index', title: '权限测试页', name: 'accesstest_index' }
+        ]
+    },
+    {
+        path: '/component',
+        icon: 'social-buffer',
+        name: 'component',
+        title: '组件',
+        component: Main,
+        children: [
+            {
+                path: 'text-editer',
+                icon: 'compose',
+                name: 'text-editer',
+                title: '富文本编辑器',
+                component: resolve => { require(['./views/my_components/text-editer/textEditer.vue'], resolve); }
+            },
+            {
+                path: 'md-editor',
+                icon: 'pound',
+                name: 'md-editor',
+                title: 'Markdown编辑器',
+                component: resolve => { require(['./views/my_components/markdown-editor/markdown-editor.vue'], resolve); }
+            },
+            {
+                path: 'draggable-list',
+                icon: 'arrow-move',
+                name: 'draggable-list',
+                title: '可拖拽列表',
+                component: resolve => { require(['./views/my_components/draggable-list/draggable-list.vue'], resolve); }
+            },
+            {
+                path: 'file-upload',
+                icon: 'android-upload',
+                name: 'file-upload',
+                title: '文件上传',
+                component: resolve => { require(['./views/my_components/file-upload/file-upload.vue'], resolve); }
+            },
+            {
+                path: 'count-to',
+                icon: 'arrow-graph-up-right',
+                name: 'count-to',
+                title: '数字渐变',
+                component: resolve => { require(['./views/my_components/count-to/count-to.vue'], resolve); }
+            }
+        ]
+    },
+    {
+        path: '/form',
+        icon: 'android-checkbox',
+        name: 'form',
+        title: '表单编辑',
+        component: Main,
+        children: [
+            { path: 'artical-publish', title: '文章发布', name: 'articalpublish', icon: 'compose', component: resolve => { require(['./views/form/article-publish/article-publish.vue'], resolve); } },
+            { path: 'workflow', title: '工作流', name: 'workflow', icon: 'arrow-swap', component: resolve => { require(['./views/form/work-flow/work-flow.vue'], resolve); } }
+
+        ]
+    },
+    // {
+    //     path: '/charts',
+    //     icon: 'ios-analytics',
+    //     name: 'charts',
+    //     title: '图表',
+    //     component: Main,
+    //     children: [
+    //         { path: 'pie', title: '饼状图', name: 'pie', icon: 'ios-pie', component: resolve => { require('./views/access/access.vue') },
+    //         { path: 'histogram', title: '柱状图', name: 'histogram', icon: 'stats-bars', component: resolve => { require('./views/access/access.vue') }
+
+    //     ]
+    // },
+    {
+        path: '/tables',
+        icon: 'ios-grid-view',
+        name: 'tables',
+        title: '表格',
+        component: Main,
+        children: [
+            { path: 'dragableTable', title: '可拖拽排序', name: 'dragableTable', icon: 'arrow-move', component: resolve => { require(['./views/tables/dragable-table.vue'], resolve); } },
+            { path: 'editableTable', title: '可编辑表格', name: 'editableTable', icon: 'edit', component: resolve => { require(['./views/tables/editable-table.vue'], resolve); } },
+            { path: 'exportableTable', title: '表格导出数据', name: 'exportableTable', icon: 'code-download', component: resolve => { require(['./views/tables/exportable-table.vue'], resolve); } },
+            { path: 'table2image', title: '表格转图片', name: 'table2image', icon: 'images', component: resolve => { require(['./views/tables/table-to-image.vue'], resolve); } }
+        ]
+    },
+    {
+        path: '/error-page',
+        icon: 'android-sad',
+        title: '错误页面',
+        name: 'errorpage',
+        component: Main,
+        children: [
+            { path: 'index', title: '错误页面', name: 'errorpage_index', component: resolve => { require(['./views/error_page/error-page.vue'], resolve); } }
+        ]
+    }
+];
+
+export const routers = [
+    loginRouter,
+    otherRouter,
+    preview,
+    locking,
+    ...appRouter,
+    page500,
+    page401,
+    page404
+];
